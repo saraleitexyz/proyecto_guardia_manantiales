51acb82b8617af983a1e5698f4a96c9b3d29eb83
Skyzo d <darioacevedohijano89@gmail.com>
2025-12-12 22:13:23 +0100

    Modificación del docker

--- 06_docker_deployment/db-init/01-schema.sql
+++ 06_docker_deployment/db-init/01-schema.sql
@@ -123,6 +123,31 @@ CREATE TABLE `profesores` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_tutor_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO profesores (id, nombre, apellidos) VALUES
+('p001', 'Jorge', 'Camacho Vela'),
+('p002', 'Francisco', 'Villalba Sánchez'),
+('p003', 'Dulcenombre de María', 'Pineda Dorado'),
+('p004', 'Alberto', 'García Díaz'),
+('p005', 'Nerea', 'Contreras Lizarreta');
+
+INSERT INTO clases (nivel, tipo, letra, id_tutor) VALUES
+(1, 'ASIR', 'A', 'p004'),
+(2, 'ASIR', 'B', 'p005');
+
+INSERT INTO guardias (dia, hora, id_tutor) VALUES
+('Lunes',  '01', 'p004'),
+('Lunes',  '02', 'p004'),
+('Martes', '01', 'p005'),
+('Martes', '02', 'p005');
+
+INSERT INTO ausencias
+(id_profesor_ausencia, id_sustituto, dia_fk, hora_fk, id_tutor_guardia_fk, id_clase)
+VALUES
+('p001', 'p004', 'Lunes',  '01', 'p004', 1),
+('p002', 'p005', 'Martes', '01', 'p005', 2),
+('p003', 'p004', 'Lunes',  '02', 'p004', 1);
+
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
